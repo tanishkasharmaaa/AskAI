b3f46c3eaea1e0807c0d85a6973182296251463c
Tanishka Sharma <anjanasharma8448766174@gmail.com>
2025-07-04 21:25:10 +0530

    done

--- Backend/config/passport.js
+++ Backend/config/passport.js
@@ -6,7 +6,7 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:"http://localhost:4000/auth/google/callback"
+    callbackURL:"https://askai-50ai.onrender.com/auth/google/callback"
 },async(accessToken,refreshToken,profile,done)=>{
    
     try {

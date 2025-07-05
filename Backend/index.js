5fceefdb3367eda1288ce9646a79323c25b70d6f
Tanishka Sharma <anjanasharma8448766174@gmail.com>
2025-07-05 11:00:42 +0530

    done

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,10 @@ require("./config/passport")
 require("dotenv").config();
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173", 
+  credentials: true }
+))
 app.use(cookieParser())
 app.use(express.json())
 

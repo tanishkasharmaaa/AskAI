e11cbb66fcdc8d2ae36120d365b01644db443cf9
Tanishka Sharma <anjanasharma8448766174@gmail.com>
2025-07-07 21:33:10 +0530

    done

--- Backend/routes/uploadAndEditImage.js
+++ Backend/routes/uploadAndEditImage.js
@@ -5,7 +5,7 @@ const upload = require("../middleware/uploadMiddleware");
 const {editImage, updateEditPrompt, displayAll} = require("../controllers/editImage");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.post("/",authMiddleware,upload.single("image"),editImage);
+router.post("/editimage",authMiddleware,upload.single("image"),editImage);
 router.patch("/edit/:id",authMiddleware,updateEditPrompt)
 router.get("/edits",authMiddleware,displayAll)
 
